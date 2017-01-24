a93ab112ade3eb8677dd0c7149ae69fe92dea608
Zachary Bennett <zbennett10@hotmail.com>
2017-01-24 08:18:20 -0500

    beginning of app

--- RecipeBox/src/App.js
+++ RecipeBox/src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//fake data mimicking local storage
+var recipes = [
+    {
+      name: 'Pumpkin Pie',
+      ingredients: ['Pumpkin', 'Crust', 'Whipped Cream']
+    }, 
+    
+    {
+      name: 'Enchiladas',
+      ingredients: ['Beans', 'Rice', 'Tortillas']
+    }, 
+
+    {
+      name: 'Spaghetti',
+      ingredients: ['Noodles', 'Sauce', 'Garlic Bread']
+    }];
 
 class App extends Component {
-
+  
+  
 
   render() {
     return (
@@ -13,9 +30,39 @@ class App extends Component {
           <h2>Welcome to React Recipe Box</h2>
         </div>
         
+        <RecipeContainer />
       </div>
     );
   }
 }
+
+var RecipeContainer = React.createClass({
+  getInitialState: function() {
+    return {recipeList: recipes}
+  },
+
+  render: function() {
+    return (
+      //foreach recipe in list state - create recipe with its properties.
+      <div className="container">
+        {this.state.recipeList.map(recipe => {
+          return <Recipe name={recipe.name}/>
+        })}
+      </div>
+    );
+  }
+});
+
+var Recipe = React.createClass({
+  
+  render: function() {
+    return (
+      //foreach bit of recipe, create an item
+      <ul>
+        <li>{this.props.name}</li>
+      </ul>
+    );
+  }
+});
 
 export default App;
